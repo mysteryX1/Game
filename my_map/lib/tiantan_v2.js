4029c1d41a721119f5515fd523f53fe20fdd578c
陈则安 <104758948+mysteryX1@users.noreply.github.com>
2024-12-23 20:21:15 +0800

    Add files via upload

---
+++ my_map/lib/tiantan_v2.js
@@ -0,0 +1,68 @@
+var sheshi = [{
+    name: '停车场',
+    position: [116.418563, 39.883041],
+    icon: 'https://a.amap.com/jsapi_demos/static/resource/img/停车场.png'
+}, {
+    name: '洗手间',
+    position: [116.415419, 39.883125],
+    icon: 'https://a.amap.com/jsapi_demos/static/resource/img/洗手间.png'
+}, {
+    name: '洗手间',
+    position: [116.410226, 39.88364],
+    icon: 'https://a.amap.com/jsapi_demos/static/resource/img/洗手间.png'
+}, {
+    name: '洗手间',
+    position: [116.412247, 39.887258],
+    icon: 'https://a.amap.com/jsapi_demos/static/resource/img/洗手间.png'
+}, {
+    name: '洗手间',
+    position: [116.412701, 39.874841],
+    icon: 'https://a.amap.com/jsapi_demos/static/resource/img/洗手间.png'
+}, {
+    name: '售票处',
+    position: [116.412442, 39.887925],
+    icon: 'https://a.amap.com/jsapi_demos/static/resource/img/门票.png'
+}, {
+    name: '售票处',
+    position: [116.413039, 39.874522],
+    icon: 'https://a.amap.com/jsapi_demos/static/resource/img/门票.png'
+}]
+
+var touristSpots = [{
+    name: '双峰插云',
+    position: [120.13255,30.245954],
+    zIndex: 300,
+    smallIcon: 'yun.png',
+    bigIcon: 'yun.png',
+    size: [300,125],
+    anchor: 'bottom-center',
+}, {
+    name: '孤山寺',
+    position: [120.14136,30.251446],
+    smallIcon: 'si.png',
+    bigIcon: 'si.png',
+    size: [300, 275],
+    anchor: 'bottom-center',
+}, {
+    name: '平湖秋月',
+    position: [120.146083,30.252336],
+    smallIcon: 'hu.png',
+    bigIcon: 'hu.png',
+    size: [300, 300],
+    anchor: 'bottom-center',
+}, {
+    name: '白堤',
+    position: [120.149369,30.256236],
+    zIndex: 200,
+    smallIcon: 'di.png',
+    bigIcon: 'di.png',
+    size: [400, 100],
+    anchor: 'bottom-center',
+}, {
+    name: '望湖楼',
+    position: [120.154716,30.260587],
+    smallIcon: 'lou.png',
+    bigIcon: 'lou.png',
+    size: [200, 300],
+    anchor: 'bottom-center',
+}];
